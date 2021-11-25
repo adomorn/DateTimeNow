c29efa2871cef1bd82bcbc2c319850a9e3ed4612
Arda Terekeci <x_anthos@hotmail.com>
2021-11-25 20:07:26 +0300

    v1.1

---
+++ DateTimeNow/DateTimeNow.CodeFixes/DateTimeNowCodeFixProvider.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace DateTimeNow
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DateTimeNowCodeFixProvider)), Shared]
+    public class DateTimeNowCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds =>
+            ImmutableArray.Create(DateTimeNowAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf()
+                .OfType<MemberAccessExpressionSyntax>().First();
+            var now = declaration.Name.ToString() == "Now" ? DateTime.Now : DateTime.UtcNow;
+
+            // Register a code action that will invoke the fix.
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                    createChangedDocument: c => ToObjectCreation(context.Document, declaration, now, c),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        private async Task<Document> ToObjectCreation(Document document, MemberAccessExpressionSyntax node,
+            DateTime now, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var args = new List<ArgumentSyntax>()
+            {
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Year))),
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Month))),
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Day))),
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Hour))),
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Minute))),
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Second))),
+                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                    SyntaxFactory.Literal(now.Millisecond)))
+            };
+            if (now.Kind == DateTimeKind.Utc)
+            {
+                args.Add(
+                    SyntaxFactory.Argument(SyntaxFactory.MemberAccessExpression(
+                        SyntaxKind.SimpleMemberAccessExpression,
+                        SyntaxFactory.IdentifierName(nameof(DateTimeKind)),
+                        SyntaxFactory.IdentifierName(nameof(DateTimeKind.Utc)))));
+            }
+
+            var dateObjectCreatingSyntax = SyntaxFactory
+                .ObjectCreationExpression(SyntaxFactory.ParseTypeName(nameof(DateTime)))
+                .WithArgumentList(
+                    SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList<ArgumentSyntax>(args))
+                ).WithAdditionalAnnotations(Formatter.Annotation);
+
+            var newRoot = root.ReplaceNode(node, dateObjectCreatingSyntax);
+
+
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
---
+++ DateTimeNow/DateTimeNow/DateTimeNowAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace DateTimeNow
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DateTimeNowAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "DateTimeNow";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(
+            nameof(Resources.AnalyzerDescription),
+            Resources.ResourceManager,
+            typeof(Resources));
+        private const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Info,
+            true,
+            Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.SimpleMemberAccessExpression);
+        }
+        private static readonly string[] InvalidTokens =
+        {
+            "System.DateTime.Now",
+            "System.DateTime.UtcNow"
+        };
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var name = (context.Node as MemberAccessExpressionSyntax)?.Name.ToString();
+            if (name == "Now" || name == "UtcNow")
+            {
+                var symbol = context.SemanticModel.GetSymbolInfo(context.Node).Symbol as IPropertySymbol;
+                var symbolText = symbol?.ToString();
+                if (InvalidTokens.Contains(symbolText))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation(), Description);
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+
+
+    }
+}
